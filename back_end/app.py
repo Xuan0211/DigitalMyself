4e32078ace953322cbd3f762089c9c789c9f0bac
Xuan0211 <2022134346@qq.com>
2023-11-29 20:53:46 +0800

    [add] simple back_end

---
+++ back_end/app.py
@@ -0,0 +1,19 @@
+﻿from flask import Flask,request
+from flask_cors import CORS
+import json
+from sendToOpenAI import sendMsgToOpenAI
+app = Flask(__name__)
+# 解决跨域问题
+CORS(app)
+
+
+@app.route('/manager/sendMsg', methods=['GET', 'POST'])
+def hi():
+    getData = request.get_json()
+    res = sendMsgToOpenAI(getData)
+    data = {
+        "ans": res,
+    }
+    res_json = json.dumps(data)
+
+    return res_json
---
+++ back_end/sendToOpenAI.py
@@ -0,0 +1,21 @@
+﻿def sendMsgToOpenAI(msg):  
+	# form ./OPENAIKEY.py import OPENAI_API_KEY
+	# you should import your own OPENAI_API_KEY
+	from OPENAIKEY import OPENAI_API_KEY
+
+	from langchain.chains import LLMChain
+	from langchain.llms import OpenAI
+	from langchain.prompts import PromptTemplate
+
+	template = """Question: {question}
+
+	Answer: Let's think step by step."""
+
+	prompt = PromptTemplate(template=template, input_variables=["question"])
+
+	llm = OpenAI(openai_api_key=OPENAI_API_KEY)
+	chain = LLMChain(llm=llm, prompt=prompt)
+	question = msg
+	ans = chain.run(question)
+	print(ans)
+	return ans

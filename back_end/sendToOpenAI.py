bc45bbeb02333a24e3b5c5432052ba90953d2219
Xuan0211 <2022134346@qq.com>
2023-12-02 17:13:41 +0800

    [fix] delete test code

--- back_end/sendToOpenAI.py
+++ back_end/sendToOpenAI.py
@@ -8,22 +8,22 @@
 	from langchain.llms import OpenAI
 	from langchain.prompts import PromptTemplate
 
-	template = """Question: {question}
-
-	Answer: Let's think step by step."""
 	template1 = """Please answer based on the following content,and if you cannot answer, do not fabricate, simply answer 'I don't know'.
 	'\n'or'\\n' in content means a newline
     Context:{content}
     Question: {question}
     Answer:	"""
 	
-	#prompt = PromptTemplate(template=template, input_variables=["question"])
 	prompt = PromptTemplate(template=template1, input_variables=["content", "question"])
 
 	llm = OpenAI(openai_api_key=OPENAI_API_KEY)
 	chain = LLMChain(llm=llm, prompt=prompt)
 	question = msg
-	content = localSearch(msg)[0]
+	content = ''
+	localContent = localSearch(msg)
+	for row in range(len(localContent)):
+		content += localContent[row] + '\n'
+	print("local search back: "+ content)
 	ans = chain.run(question=question, content=content)
 	print(ans)
 	return ans

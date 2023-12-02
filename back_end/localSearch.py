d41add9f6e648a5a956f46209d28fbd7454aaa8e
Xuan0211 <2022134346@qq.com>
2023-12-02 17:15:13 +0800

    [fix] link to databank

--- back_end/localSearch.py
+++ back_end/localSearch.py
@@ -1,36 +1,37 @@
-﻿# -*- coding: utf-8 -*-
-"""
-	@file:localSearch.py
-	@author:Xuan(2022134346@qq.com)
-	@description: 本地文本匹配搜索
-"""
-def localSearch(problem: str):  
+﻿
 
-	import sys
-	import pandas as pd
-	from pathlib import Path
-	from similarities import BertSimilarity
-	sys.path.append('..')
+def localSearch(problem: str):
+    import sys
+    import pandas as pd
+    from pathlib import Path
+    from similarities import BertSimilarity
+    sys.path.append('..')
+    LOCAL_PATH = str(Path(__file__).parent)
 
-	LOCAL_PATH = str(Path(__file__).parent)
+    # 1. Read all csv files in the output folder
+    output_folder = LOCAL_PATH + '/store/databank/output'
+    csv_files = Path(output_folder).rglob('*.csv')
+    dfs = []
+    for file in csv_files:
+        df = pd.read_csv(file, header=None)
+        dfs.append(df)
 
-	df = pd.read_csv(LOCAL_PATH + '/store/test.csv', header=None)
+    # 2. Concatenate all DataFrames into a single DataFrame
+    concatenated_df = pd.concat(dfs)
 
-	# 1.Compute cosine similarity between two sentences.
-	sentences = [problem]
-	corpus = df.iloc[:, 0].tolist()
-	model = BertSimilarity(model_name_or_path="shibing624/text2vec-base-chinese")
+    # 3. Compute cosine similarity between two sentences
+    sentences = [problem]
+    corpus = concatenated_df.iloc[:, 0].tolist()
+    model = BertSimilarity(model_name_or_path="shibing624/text2vec-base-chinese")
+    similarity_scores = model.similarity(sentences, corpus)
 
-	# 2.Compute similarity between two list
-	similarity_scores = model.similarity(sentences, corpus)
+    # 4. Semantic Search
+    model.add_corpus(corpus)
 
-	# 3.Semantic Search
-	model.add_corpus(corpus)
- 
-	res = []
-	for i in list(model.search(sentences[0], topn=3)[0].keys()):
-		res.append(corpus[i])
-	return res
+    res = []
+    for i in list(model.search(sentences[0], topn=3)[0].keys()):
+        res.append(corpus[i])
+    return res
 
 if __name__ == '__main__':
-    print(localSearch('软件工程生命周期什么？'))
+    print(localSearch('参考书是什么'))

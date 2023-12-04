d17a651f1ce4206ab47edc74f121f49b35cd7fbc
Xuan0211 <2022134346@qq.com>
2023-12-04 16:15:47 +0800

    [add] globalSetting

---
+++ back_end/globalSetting.py
@@ -0,0 +1,11 @@
+﻿from pathlib import Path
+LOCAL_PATH = str(Path(__file__).parent)
+INPUT_FOLDER: str = LOCAL_PATH + '\\store\\databank\\input\\'
+OUTPUT_FOLDER: str = LOCAL_PATH + '\\store\\databank\\output\\'
+STORE_FOLDER: str = LOCAL_PATH + '\\store\\'
+DB_FOLDER: str = LOCAL_PATH + '\\store\\database\\'
+
+from enum import Enum
+class FileState(Enum):
+    DISENABLE = 0    
+    ENABLE = 1
